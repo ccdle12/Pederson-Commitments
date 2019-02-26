5e1d9ee3a39902a08bd207f64f717aad9978d623
ccdle12 <chris.coverdale24@gmail.com>
2019-02-26 20:13:22 +0000

    python: adding homomorphic encryption

--- python/pederson_commitments.py
+++ python/pederson_commitments.py
@@ -2,19 +2,17 @@ import unittest
 from Crypto import Random
 from Crypto.Random import random
 from Crypto.Util import number
+from functools import reduce
 
 class Verifier:
     def __init__(self):
         pass
 
     # Validate a commitment according to the message, h and r.
-    def validate(self, c, g, m, h, r, q):
-        if c == ((g**m % q) * (h**r % q) % q):
-            return True
-        
-        return False
-
-
+    def validate(self, c, g, m, h, q, *r):
+        r_total = reduce((lambda x, y: x + y), r)
+        return c == ((g**m % q) * (h**r_total % q) % q)
+    
 class Sender:
     def __init__(self, q=None, g=None, s=None, h=None):
         self.Q = q
@@ -41,6 +39,16 @@ class Sender:
         c = (pow(self.G, m, self.Q) * pow(self.H, r, self.Q)) % self.Q
 
         return c, r
+
+    # Add the commitments together using multiplication.
+    # *cm is the equivalent to variadic declaration.
+    def add(self, *cm):
+        total = 1
+        for x in cm:
+            total *= x
+
+        total = total % self.Q
+        return total
 
 class SenderTest(unittest.TestCase):
     # Test that we can create an instance of Sender with pre-determined
@@ -74,8 +82,32 @@ class SenderTest(unittest.TestCase):
 
         # Validate the commitment.
         verifier = Verifier()
-        valid_commitment = verifier.validate(c, sender.G, msg, sender.H, r, sender.Q)
+        valid_commitment = verifier.validate(c, sender.G, msg, sender.H, sender.Q, r)
         self.assertTrue(valid_commitment) 
+
+    # Testing Homomorphic Encryption.
+    # We can use the same principle for a pederson commitment to perform
+    # homomorphic encryption.
+    def test_homomorphic_encryption(self):
+        sender = Sender().new_with_args(q=11, g=7, s=5)
+
+        # Create two messages to combine.
+        msg1 = 13
+        msg2 = 17
+        
+        # Create x2 commitments and random value.
+        c1, r1 = sender.create_commitment(msg1)
+        c2, r2 = sender.create_commitment(msg2)
+        
+        # Multiply the the two commitments.
+        combined_cm = sender.add(c1, c2)
+
+        # We can provide a combined commitment, plus both messages and random values to be verified.
+        verifier = Verifier()
+        valid_commitment = verifier.validate(combined_cm, sender.G, msg1 + msg2, sender.H, sender.Q, r1, r2)
+        self.assertTrue(valid_commitment) 
+
+
 
 if __name__ == "__main__":
     unittest.main()
